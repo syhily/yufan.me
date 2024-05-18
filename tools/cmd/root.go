eed3d222035ccc189dd8796c36b00deeba6eeef5
Yufan Sheng <syhily@gmail.com>
2024-05-18 19:54:44 +0800

    chore: migrate the static files from the old blog.

---
+++ tools/cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "tools",
+	Short: "A set of useful tools for writing in weblog",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
