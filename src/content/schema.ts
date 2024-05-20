361a67e5776fdf77eefe3aa96be1de9fd3191e36
Yufan Sheng <syhily@gmail.com>
2024-05-21 00:30:36 +0800

    feat: add the schema validation for the astro contents.

--- src/content/schema.ts
+++ src/content/schema.ts
@@ -9,62 +9,113 @@ const postsCollection = await getCollection('posts');
 const tagsCollection = await getCollection('tags');
 
 // Redefine the types from the astro content.
-type Category = (typeof categoriesCollection)[number]['data'] & { counts: number; permalink: string };
-type Friend = (typeof friendsCollection)[number]['data'][number];
-type Options = (typeof optionsCollection)[number]['data'];
-type Page = (typeof pagesCollection)[number]['data'] & {
+export type Category = (typeof categoriesCollection)[number]['data'] & { counts: number; permalink: string };
+export type Friend = (typeof friendsCollection)[number]['data'][number];
+export type Options = (typeof optionsCollection)[number]['data'];
+export type Page = (typeof pagesCollection)[number]['data'] & {
   slug: string;
   permalink: string;
   render: () => Render['.mdx'];
 };
-type Post = (typeof postsCollection)[number]['data'] & {
+export type Post = (typeof postsCollection)[number]['data'] & {
   slug: string;
   permalink: string;
   render: () => Render['.mdx'];
 };
-type Tag = (typeof tagsCollection)[number]['data'][number] & { counts: number; permalink: string };
-
-// Export all the types.
-export type { Category, Friend, Options, Page, Post, Tag };
+export type Tag = (typeof tagsCollection)[number]['data'][number] & { counts: number; permalink: string };
 
 // Translate the Astro content into the original content for dealing with different configuration types.
-const friends: Friend[] = friendsCollection[0].data;
-
-const options: Options = optionsCollection[0].data;
-
-const pages: Page[] = pagesCollection.map((page) => ({
-  slug: page.slug,
-  permalink: `/${page.slug}`,
-  render: async () => {
-    const entry = await getEntryBySlug('pages', page.slug);
-    return entry.render();
-  },
-  ...page.data,
-}));
-
-const posts: Post[] = postsCollection.map((post) => ({
-  slug: post.slug,
-  permalink: `/posts/${post.slug}`,
-  render: async () => {
-    const entry = await getEntryBySlug('posts', post.slug);
-    return entry.render();
-  },
-  ...post.data,
-}));
-
-const categories: Category[] = categoriesCollection.map((cat) => ({
+export const friends: Friend[] = friendsCollection[0].data;
+export const options: Options = optionsCollection[0].data;
+export const pages: Page[] = pagesCollection
+  .filter((page) => page.data.published || !import.meta.env.PROD)
+  .map((page) => ({
+    slug: page.slug,
+    permalink: `/${page.slug}`,
+    render: async () => {
+      const entry = await getEntryBySlug('pages', page.slug);
+      return entry.render();
+    },
+    ...page.data,
+  }));
+export const posts: Post[] = postsCollection
+  .filter((post) => post.data.published || !import.meta.env.PROD)
+  .map((post) => ({
+    slug: post.slug,
+    permalink: `/posts/${post.slug}`,
+    render: async () => {
+      const entry = await getEntryBySlug('posts', post.slug);
+      return entry.render();
+    },
+    ...post.data,
+  }))
+  .sort((left: Post, right: Post) => {
+    const a = left.date.getTime();
+    const b = right.date.getTime();
+    return options.settings.post.sort === 'asc' ? a - b : b - a;
+  });
+export const categories: Category[] = categoriesCollection.map((cat) => ({
   counts: posts.filter((post) => post.category === cat.data.name).length,
   permalink: `/cats/${cat.data.slug}`,
   ...cat.data,
 }));
-
-const tags: Tag[] = tagsCollection[0].data.map((tag) => ({
+export const tags: Tag[] = tagsCollection[0].data.map((tag) => ({
   counts: posts.filter((post) => post.tags.includes(tag.name)).length,
   permalink: `/tags/${tag.slug}`,
   ...tag,
 }));
 
-export { friends, options, pages, posts, categories, tags };
+// Find the missing categories from posts.
+const missingCategories: string[] = posts
+  .map((post) => post.category)
+  .filter((c) => !categories.find((cat) => cat.name === c));
+if (missingCategories.length > 0) {
+  throw new Error(`The bellowing categories has not been configured:\n$${missingCategories.join('\n')}`);
+}
+
+// Find the missing tags from posts.
+const missingTags: string[] = posts.flatMap((post) => post.tags).filter((tag) => !tags.find((t) => t.name === tag));
+if (missingTags.length > 0) {
+  throw new Error(`The bellowing tags has not been configured:\n$${missingTags.join('\n')}`);
+}
+
+// Find the missing covers from posts.
+const missingCovers = posts
+  .filter((post) => post.cover === '/images/default-cover.jpg')
+  .map((post) => ({ title: post.title, slug: post.slug }));
+if (process.env.NODE_ENV !== 'production' && missingCovers.length > 0) {
+  // We only warn here for this is a known improvement.
+  console.warn(`The following ${missingCovers.length} posts don't have a cover.`);
+  console.warn(missingCovers);
+}
+
+// Validate the posts and pages' slug. They should be unique globally.
+const postsSlugs = new Set<string>();
+for (const post of posts) {
+  if (postsSlugs.has(post.slug)) {
+    throw new Error(`Duplicate post slug: ${post.slug}`);
+  }
+  postsSlugs.add(post.slug);
+}
+for (const page of pages) {
+  if (postsSlugs.has(page.slug)) {
+    throw new Error(`Page and post share same slug: ${page.slug}`);
+  }
+}
+
+// Validate feature posts option.
+const featurePosts: string[] = options.settings.post.feature ?? [];
+const invalidFeaturePosts = featurePosts.filter((slug) => !postsSlugs.has(slug));
+if (invalidFeaturePosts.length > 0) {
+  throw new Error(`The bellowing feature posts are invalid:\n$${invalidFeaturePosts.join('\n')}`);
+}
+
+// Validate pinned categories.
+const pinnedCategories: string[] = options.settings.post.category ?? [];
+const invalidPinnedCategories = pinnedCategories.filter((c) => categories.find((e) => e.name === c));
+if (invalidPinnedCategories.length > 0) {
+  throw new Error(`The bellowing pinned categories are invalid:\n$${invalidPinnedCategories.join('\n')}`);
+}
 
 export const getPost = (slug: string): Post | undefined => {
   return posts.find((post) => post.slug === slug);
