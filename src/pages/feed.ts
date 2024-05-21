e48723c934fdc1df4180bb835a0f6159ff09c106
Yufan Sheng <syhily@gmail.com>
2024-05-22 05:31:32 +0800

    feat: add the feed page.

---
+++ src/pages/feed.ts
@@ -0,0 +1,21 @@
+import { options, posts } from '@/content/schema';
+import rss from '@astrojs/rss';
+
+export const GET = async () => {
+  // TODO Support full content {options.settings.feed.full}
+  // See https://github.com/withastro/roadmap/discussions/419
+
+  return rss({
+    title: options.title,
+    description: options.description,
+    site: options.website,
+    items: posts.slice(0, options.settings.feed.size).map((post) => ({
+      link: options.website + post.permalink,
+      title: post.title,
+      pubDate: post.date,
+      description: post.summary,
+      author: options.author.name,
+      categories: [post.category, ...post.tags],
+    })),
+  });
+};
