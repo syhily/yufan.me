1fc8abafc57ef0c76cedaa03d0444a08b835a6fa
Yufan Sheng <syhily@gmail.com>
2024-05-22 04:04:32 +0800

    feat: add the docker file.

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,15 @@
 import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
-import zeabur from '@zeabur/astro-adapter/serverless';
 import { defineConfig } from 'astro/config';
-
 const site = import.meta.env.PROD ? 'https://blog.yufan.me' : 'http://localhost:4321';
 
 // https://astro.build/config
 export default defineConfig({
   site: site,
-  adapter: zeabur(),
   output: 'hybrid',
   integrations: [sitemap(), mdx()],
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
