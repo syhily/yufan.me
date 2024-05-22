da9f64892ce1faf2c5b3dab7872fd42eacbdb2d4
Yufan Sheng <syhily@gmail.com>
2024-05-22 10:50:22 +0800

    feat: use server mode with ssr support.

--- astro.config.ts
+++ astro.config.ts
@@ -1,72 +1,16 @@
 import mdx from '@astrojs/mdx';
 import node from '@astrojs/node';
-import sitemap from '@astrojs/sitemap';
 import robots from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
-import type { Literal, Node, Parent } from 'unist';
-import { visit } from 'unist-util-visit';
-import { imageMetadata } from './src/utils/images';
 
+// Dynamic switch the site.
 const site = import.meta.env.PROD ? 'https://yufan.me' : 'http://localhost:4321';
-
-export type ImageNode = Parent & {
-  url: string;
-  alt: string;
-  name: string;
-  width?: number;
-  height?: number;
-  attributes: (Literal & { name: string })[];
-};
-
-export const lazyImage = () => {
-  return async (tree: Node) => {
-    const images: ImageNode[] = [];
-
-    // Find all the local image node.
-    visit(tree, 'image', (node: Node, _, parent: Parent) => {
-      const imageNode = node as ImageNode;
-      // Skip remote images.
-      if (imageNode.url.startsWith('http')) {
-        return;
-      }
-
-      images.push(imageNode);
-    });
-
-    // Process images.
-    await Promise.all(images.map(transformNextImage));
-    return tree;
-  };
-};
-
-const transformNextImage = async (imageNode: ImageNode) => {
-  const metadata = await imageMetadata(imageNode.url);
-  if (metadata == null) {
-    throw new Error(`Failed to get image metadata: ${imageNode.url}`);
-  }
-
-  // Convert original node to lazy loading.
-  imageNode.type = 'mdxJsxFlowElement';
-  imageNode.name = 'img';
-  imageNode.attributes = [
-    { type: 'mdxJsxAttribute', name: 'alt', value: imageNode.alt },
-    { type: 'mdxJsxAttribute', name: 'src', value: imageNode.url },
-    { type: 'mdxJsxAttribute', name: 'width', value: imageNode.width ?? metadata.width },
-    { type: 'mdxJsxAttribute', name: 'height', value: imageNode.height ?? metadata.height },
-    { type: 'mdxJsxAttribute', name: 'loading', value: 'lazy' },
-    {
-      type: 'mdxJsxAttribute',
-      name: 'style',
-      value: `background-image: ${metadata.style.backgroundImage}; background-position: "${metadata.style.backgroundPosition}"; background-size: "${metadata.style.backgroundSize}"; background-repeat: "${metadata.style.backgroundRepeat}"`,
-    },
-  ];
-};
 
 // https://astro.build/config
 export default defineConfig({
   site: site,
   output: 'server',
-  integrations: [sitemap(), robots({ sitemap: `${site}/sitemap-index.xml` }), mdx({ remarkPlugins: [lazyImage] })],
+  integrations: [robots({ sitemap: `${site}/sitemap.xml` }), mdx()],
   adapter: node({
     mode: 'standalone',
   }),
---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,26 @@
+import { options, pages, posts } from '@/content/schema';
+
+export async function GET() {
+  const result = `
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>${options.website}/</loc></url>
+  ${posts
+    .map((post) => {
+      return `<url><loc>${options.website}${post.permalink}</loc><lastmod>${post.date.toISOString()}</lastmod></url>`;
+    })
+    .join('\n')}
+    ${pages
+      .map((page) => {
+        return `<url><loc>${options.website}${page.permalink}</loc><lastmod>${page.date.toISOString()}</lastmod></url>`;
+      })
+      .join('\n')}
+</urlset>
+  `.trim();
+
+  return new Response(result, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
