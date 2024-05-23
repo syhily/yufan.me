567099034fd877f5192c85cd8ce295dd06e9ae08
Yufan Sheng <syhily@gmail.com>
2024-05-23 20:17:23 +0800

    feat: add container API to rss page.

--- astro.config.ts
+++ astro.config.ts
@@ -19,4 +19,7 @@ export default defineConfig({
       wrap: false,
     },
   },
+  security: {
+    checkOrigin: true,
+  },
 });
--- src/pages/feed.ts
+++ src/pages/feed.ts
@@ -1,9 +1,25 @@
+import PostContent from '@/components/page/post/PostContent.astro';
 import { options, posts } from '@/helpers/schema';
 import rss from '@astrojs/rss';
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 
 export const GET = async () => {
-  // TODO Support full content {options.settings.feed.full}
-  // See https://github.com/withastro/roadmap/discussions/419
+  const contents = new Map<string, string>();
+  if (options.settings.feed.full) {
+    const container = await AstroContainer.create();
+    const promises = posts.slice(0, options.settings.feed.size).map(async (post) => ({
+      key: post.slug,
+      value: await container.renderToString(PostContent, {
+        params: {
+          slug: post.slug,
+        },
+      }),
+    }));
+
+    for (const { key, value } of await Promise.all(promises)) {
+      contents.set(key, value);
+    }
+  }
 
   return rss({
     title: options.title,
@@ -16,6 +32,7 @@ export const GET = async () => {
       pubDate: post.date,
       description: post.summary,
       author: options.author.name,
+      content: options.settings.feed.full ? contents.get(post.slug) : undefined,
       categories: [post.category, ...post.tags],
     })),
   });
