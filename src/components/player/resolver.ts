2165ea262c25ebe86a5eaaa3875cc4155a41a3d1
Yufan Sheng <syhily@gmail.com>
2024-06-16 20:38:28 +0800

    fix: the netease music player issue.

--- src/components/player/resolver.ts
+++ src/components/player/resolver.ts
@@ -16,12 +16,10 @@ const emptySong = { name: '', artist: '', url: '', pic: '' };
 const song = async (props: MusicPlayerProps): Promise<Song> => {
   const { netease, song } = props;
 
-  if (import.meta.env.DEV) {
-    // Fix the UNABLE_TO_GET_ISSUER_CERT_LOCALLY issue.
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-  }
-
   if (netease) {
+    // Fix the UNABLE_TO_GET_ISSUER_CERT_LOCALLY issue for the mirror site.
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
     // https://github.com/injahow/meting-api
     const data = await fetch(`https://api.injahow.cn/meting/?type=song&id=${netease}`)
       .then((response) => response.json())
@@ -30,7 +28,8 @@ const song = async (props: MusicPlayerProps): Promise<Song> => {
         return [emptySong];
       });
 
-    return data[0] as Song;
+    // Check the return result.
+    return { name: data[0].name, artist: data[0].artist, url: data[0].url, pic: data[0].pic };
   }
 
   if (song) {
--- tools/go.mod
+++ tools/go.mod
@@ -4,11 +4,11 @@ go 1.22
 
 require (
 	github.com/disintegration/imaging v1.6.2
-	github.com/spf13/cobra v1.8.0
+	github.com/spf13/cobra v1.8.1
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/image v0.16.0 // indirect
+	golang.org/x/image v0.17.0 // indirect
 )
