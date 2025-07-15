092974bac984548c84621493f2e2bdfe04e274b2
Yufan Sheng <syhily@gmail.com>
2025-07-15 15:58:27 +0800

    feat: add extra logging for auth actions.

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -4,6 +4,16 @@ import { validateToken } from '@/helpers/auth/csrf'
 import { login } from '@/helpers/auth/session'
 import { createAdmin, hasAdmin } from '@/helpers/auth/user'
 import { exceedLimit, incrLimit } from '@/helpers/cache'
+import options from '@/options'
+
+function loginLog({ email, clientAddress, request, success }: { email: string, clientAddress: string, request: Request, success: boolean }): void {
+  if (success) {
+    console.warn(`Successfully login into the ${options.website}`, { email, clientAddress, userAgent: request.headers.get('User-Agent') })
+  }
+  else {
+    console.error(`Failed to login into the ${options.website}`, { email, clientAddress, userAgent: request.headers.get('User-Agent') })
+  }
+}
 
 export const auth = {
   signUpAdmin: defineAction({
@@ -56,6 +66,7 @@ export const auth = {
     }),
     handler: async ({ email, password, token }, { session, clientAddress, request }) => {
       if (session === undefined) {
+        loginLog({ email, clientAddress, request, success: false })
         throw new ActionError({
           code: 'INTERNAL_SERVER_ERROR',
           message: 'Please configure your astro session store',
@@ -63,6 +74,7 @@ export const auth = {
       }
       const [valid, error] = await validateToken(session, token)
       if (!valid) {
+        loginLog({ email, clientAddress, request, success: false })
         throw new ActionError({
           code: 'UNAUTHORIZED',
           message: error,
@@ -70,6 +82,7 @@ export const auth = {
       }
 
       if (await exceedLimit(clientAddress)) {
+        loginLog({ email, clientAddress, request, success: false })
         throw new ActionError({
           code: 'TOO_MANY_REQUESTS',
           message: 'Too many failed in login, lock for 30 minutes',
@@ -84,6 +97,7 @@ export const auth = {
           message: 'Invalid login credential.',
         })
       }
+      loginLog({ email, clientAddress, request, success })
     },
   }),
 }
