bf40c893cb16f78f2cd8503ff5c5b5869a77b586
Yufan Sheng <syhily@gmail.com>
2025-10-24 07:16:16 +0800

    chore: speed up the rendering at the first time.

--- src/helpers/content/search.ts
+++ src/helpers/content/search.ts
@@ -1,9 +1,5 @@
 import type { DocumentData } from 'flexsearch'
-import { REDIS_URL } from 'astro:env/server'
 import { Document } from 'flexsearch'
-import Database from 'flexsearch/db/redis'
-import { createClient } from 'redis'
-import { formatLocalDate } from '@/helpers/content/formatter'
 import { getPosts } from '@/helpers/content/schema'
 
 interface PostItem extends DocumentData {
@@ -11,13 +7,10 @@ interface PostItem extends DocumentData {
   slug: string
   raw: string
   tags: string[]
-  updated: string
 }
 
 // Create indexes on top of Redis database
-const redis = await createClient({ url: REDIS_URL }).connect()
-const database = new Database('posts-search-indexes', { db: redis })
-const index = new Document<PostItem, WorkerType, Database>({
+const index = new Document<PostItem>({
   tokenize: 'full',
   document: {
     id: 'slug',
@@ -25,34 +18,16 @@ const index = new Document<PostItem, WorkerType, Database>({
     tag: 'tags',
   },
 })
-await index.mount(database)
 
 // Start indexing posts
-for (const post of getPosts({ hidden: true, schedule: true })) {
-  const existPost = await index.get(post.slug)
-  const updateDate = formatLocalDate(post.updated ?? new Date())
-
-  if (existPost !== null) {
-    if (existPost.updated !== updateDate) {
-      await index.update({
-        title: post.title,
-        slug: post.slug,
-        raw: (await post.raw()) || post.summary,
-        tags: post.tags,
-        updated: updateDate,
-      })
-    }
-  }
-  else {
-    await index.add({
-      title: post.title,
-      slug: post.slug,
-      raw: (await post.raw()) || post.summary,
-      tags: post.tags,
-      updated: updateDate,
-    })
-  }
-}
+await Promise.all(getPosts({ hidden: true, schedule: true }).map(async (post) => {
+  index.add({
+    title: post.title,
+    slug: post.slug,
+    raw: (await post.raw()) || post.summary,
+    tags: post.tags,
+  })
+}))
 
 export async function searchPosts(
   query: string,
@@ -63,7 +38,7 @@ export async function searchPosts(
   page: number
   totalPages: number
 }> {
-  const ids = (await index.search(query)).flatMap(({ result }) => result.map(id => id.toString()))
+  const ids = index.search(query).flatMap(({ result }) => result.map(id => id.toString()))
   const totalHits = [...new Set(ids)]
   return {
     hits: totalHits.slice(offset, offset + limit),
